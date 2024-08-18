667411993bee2425a0156d3dccf4fef2b7c7ffb2
jacobtt21 <jacobt1206@gmail.com>
2024-08-18 17:34:34 -0500

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,54 @@ import "./global.css";
 
 import { useState, useEffect } from "react";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./dialog";
+
 const StartBlock = ({ publicKey } : {publicKey: string}) => {
-  const [block, setBlock] = useState<string | null>("hi Pussy");
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+
+  async function fetchJobs() {
+    let headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+
+    const response = await fetch(`http://localhost:3001/api/jobs?pk=${publicKey}`, {
+      method: "GET",
+      headers: headers
+    });
+  }
+
+  useEffect(() => {
+    if (dialogOpen) {
+      fetchJobs();
+    }
+
+  }, [dialogOpen]);
 
   return (
     <div className="p-4 text-white bg-black">
-      {block}
-      {publicKey}
+      <h1 className="text-2xl font-bold">Start Block</h1>
+      <Dialog
+      open={dialogOpen}
+      onOpenChange={() => setDialogOpen(!dialogOpen)}
+      >
+        <DialogTrigger>Open!</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
